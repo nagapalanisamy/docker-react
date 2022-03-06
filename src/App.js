5eb373c9272359fbaffe1e7955538c538fbbcf93
Naga Palanisamy <spantechnology@MacBook-Pro-2.local>
2022-03-06 18:30:18 +0530

    changed app text

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello There Naga</p>
+        <p>Hello There Naga - now on new feature branch</p>
         <a
           className="App-link"
           href="https://reactjs.org"
